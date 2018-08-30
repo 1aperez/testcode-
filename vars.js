8f11466c74eba3d784999373c32536491420ce8f
1aperez <42749606+1aperez@users.noreply.github.com>
2018-08-30 13:55:51 -0500

    fix vars

---
+++ vars.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = -25;
+//create two more variables c and d and give them whatever number you want.
+let c = 15;
+let d = 62;
+const total = a+b+c+d;
+console.log(total);
+
+
+
+
+//new section on string variables
+let convo = "hello ";
+//set name to your name, first and last.
+let name = "Angel Perez"; 
+//add string convo and name together.
+const greeting = (convo + name); 
+console.log(greeting);
+
+//make your own message similar to what is above.
+let convo2 = "Pog";
+let x = "Champ";
+const Poggers(convo2 + x);
+console.log(Poggers);
